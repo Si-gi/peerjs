949069137ba861afcde7b2b47389ddeacd05348f
Gilbert Simon <si_gil@live.fr>
2020-09-29 15:43:06 +0100

    screen sharing & webcam

---
+++ fin_septembre/public/script.js
@@ -0,0 +1,203 @@
+const socket = io('/')
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+})
+var myUserId = "";
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+
+  
+const videoGrid = document.getElementById('video-grid')
+
+
+
+//const myScreen = document.createElement('video');
+//myScreen.muted = true;
+
+const myVideo = document.createElement('video');
+myVideo.muted = true
+const peers = {};
+
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addCamStream(myVideo, stream);
+  
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      addCamStream(video, userVideoStream)
+    })
+  })
+  
+  socket.on('user-connected', (userId) => {
+    myUserId = userId; //FIXME:
+    console.log("User Connected " + userId)
+    connectToNewUser(userId, stream)
+
+  });
+  // input value
+  let text = $("input");
+  // when press enter send message
+  $("html").keydown(function (e) {
+    if (e.which == 13 && text.val().length !== 0) {
+      socket.emit("message", text.val());
+      text.val("");
+    }
+  });
+  socket.on("createMessage", (message) => {
+    $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+    scrollToBottom();
+  });
+});
+  socket.on('user-disconnected', userId => {
+	  if (peers[userId]) peers[userId].close()
+  })
+
+
+function addCamStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
+}
+
+
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addCamStream(video, userVideoStream)
+  })
+  
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+/*function shareScreen(captureStream){
+  myPeer.on('call', call => {
+    call.answer(captureStream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+      getStream(video, userVideoStream)
+    })
+  });
+}
+*/
+
+async function screenCapture() {
+  
+  let displayMediaOptions = {
+    video: {
+      cursor: "always"
+    },
+    audio: false
+  };
+  const screenElem = document.getElementById('screen-shared');
+  
+  try {
+    captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    
+    screenElem.srcObject = captureStream ;
+    myPeer.call(myUserId, captureStream);  
+  } catch(err) {
+    console.log(err);
+  }
+  
+}    
+
+
+const scrollToBottom = () => {
+  var d = $(".main__chat_window");
+  d.scrollTop(d.prop("scrollHeight"));
+};
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const playStop = () => {
+  console.log("object");
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const exit = () => {
+  window.location.href = "https://google.com";
+};
+
+// const copyInfo = () => {
+//   navigator.clipboard.writeText(window.location.href);
+// };
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+function dumpOptionsInfo() {
+  const videoTrack = screenElem.srcObject.getVideoTracks()[0];
+ 
+  console.info("Track settings:");
+  console.info(JSON.stringify(videoTrack.getSettings(), null, 2));
+  console.info("Track constraints:");
+  console.info(JSON.stringify(videoTrack.getConstraints(), null, 2));
+}
+
+function stopCapture(evt) {
+  let tracks = screenElem.srcObject.getTracks();
+
+  tracks.forEach(track => track.stop());
+  screenElem.srcObject = null;
+}
