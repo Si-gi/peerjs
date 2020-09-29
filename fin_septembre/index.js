5790b26fb864ce7ac4ab70387a194cf8db7a09a9
Gilbert Simon <si_gil@live.fr>
2020-09-29 23:23:27 +0100

    reverse

--- fin_septembre/index.js
+++ fin_septembre/index.js
@@ -12,6 +12,40 @@ var httpsOptions = {
   cert: fs.readFileSync('conf/cert.pem')
 };
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req,res) => {
+	res.render('room',{roomId: req.params.room})
+})
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    // messages
+    socket.on("message", (message) => {
+      //send message to the same room
+      io.to(roomId).emit("createMessage", message);
+    });
+
+    socket.on("screen",(screen) => {
+      io.to(roomId).emit("screenSharing", screen)
+    })
+
+    // socket.on("share", (stream) => {
+    //   socket.to(roomId).broadcast.emit("screenShared", stream);
+    // });
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
 
 
 /*server.createServer(httpsOptions, app).listen(60000,()=>{
